954e0cbacad516f0682e413f599a554ecb9938a6
myprojects <aboolfazlkordi83@gmail.com>
2025-02-26 17:08:55 +0330

    first commit for Header section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function hangman() {
+    return (
+        <main>
+            <header>
+                <h1>Assembly:Endgame</h1>
+                <p>
+                    Guess the world within 8 attempts to keep the programming world safe from Assembly!
+                </p>
+            </header>
+        </main>
+    )
+}
